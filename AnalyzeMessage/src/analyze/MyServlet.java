26cbfe1a6f5ec0885afe3072fdd4a05a4de8e9f2
codebo <2653400637@qq.com>
2024-07-22 19:48:47 +0800

    删除版本

--- AnalyzeMessage/src/analyze/MyServlet.java
+++
@@ -1,8 +0,0 @@
-package analyze;
-
-public interface MyServlet {
-    //初始化一些信息
-    public void init();
-    //正式调用
-    public void service(MyRequest request, MyResponse response); //无论什么请求都会发给它
-}
